44904aab33367727a3de363a256803d7d0f7a445
Abrar Mahabub (Nowrid) <abrar.nowrid@gmail.com>
2024-10-04 12:31:13 +0600

    completed and hosted on hostinger

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -27,31 +27,11 @@ const Stars = (props) => {
 };
 
 const StarsCanvas = (props) => {
-  // const ref = useRef();
-  // const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 });
-
   return (
     <div className="w-full h-auto absolute inset-0 z-[-1]">
       <Canvas camera={{ position: [0, 0, 1] }}>
         <Suspense fallback={null}>
           <Stars />
-          {/* <group rotation={[0, 0, Math.PI / 4]}>
-            <Points
-              ref={ref}
-              positions={sphere}
-              stride={3}
-              frustumCulled
-              {...props}
-            >
-              <PointMaterial
-                transparent
-                color="#f272c8"
-                size={0.002}
-                sizeAttenuation={true}
-                depthWrite={false}
-              />
-            </Points>
-          </group> */}
         </Suspense>
         <Preload all />
       </Canvas>
